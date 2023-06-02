09b4b76511787fced3b77e9fd737bc69fa5bb6ec
progmamun <mamun333089@gmail.com>
2023-06-02 18:01:15 +0600

    custom-log

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,25 +1,45 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
 import path from 'path'
+const { combine, timestamp, label, prettyPrint, printf } = format
 
-const logger = winston.createLogger({
+// custom log format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hours = date.getHours()
+  const minutes = date.getMinutes()
+  const second = date.getSeconds()
+  return `${date.toString()} ${hours}:${minutes}:${second} } [${label}] ${level}: ${message}`
+})
+
+const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
       level: 'info',
     }),
   ],
 })
-const errorlogger = winston.createLogger({
+const errorlogger = createLogger({
   level: 'error',
-  format: winston.format.json(),
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat,
+    prettyPrint()
+  ),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
       level: 'error',
     }),
