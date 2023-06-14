4de0e45817c3eee7aec4105e2891bc2a30997446
progmamun <mamun333089@gmail.com>
2023-06-14 20:20:03 +0600

    [refactor]-done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,30 +1,35 @@
 import express from 'express';
+
+import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { UserRoutes } from '../modules/user/user.routes';
 import { AcademicSemesterRoutes } from '../modules/academicSemester/academicSemester.routes';
 import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
-import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
+import { StudentRoutes } from '../modules/student/student.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/users/',
+    path: '/users',
     route: UserRoutes,
   },
   {
-    path: '/academic-semesters/',
+    path: '/academic-semesters',
     route: AcademicSemesterRoutes,
   },
   {
-    path: '/academic-faculties/',
+    path: '/academic-faculties',
     route: AcademicFacultyRoutes,
   },
   {
-    path: '/academic-departments/',
+    path: '/academic-departments',
     route: AcademicDepartmentRoutes,
+  },
+  {
+    path: '/students',
+    route: StudentRoutes,
   },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
-
 export default router;
